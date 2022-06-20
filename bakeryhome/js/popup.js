1582ade519a183173b408f69f8551e537f93dc71
seyeongs <pronetp@naver.com>
2022-06-20 21:25:20 +0900

    0620

---
+++ bakeryhome/js/popup.js
@@ -0,0 +1,9 @@
+function partpopup(){
+    window.open("popup.html", "popad", "width=450, height=470");
+}
+
+function info(){
+    alert("응애 DD베이커리 \n종로에 4호점 오픈했오");
+}
+
+setTimeout(function(){info();},3000);
